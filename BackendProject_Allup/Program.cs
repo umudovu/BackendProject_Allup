437b4945be2cac262d6bd35ce59c4b3b75c4bf27
Umudov Umud <umudau@code.edu.az>
2022-07-23 23:28:07 +0400

    feat: adminpanel create

--- BackendProject_Allup/Program.cs
+++ BackendProject_Allup/Program.cs
@@ -34,14 +34,19 @@ app.UseRouting();
 app.UseAuthentication();    
 app.UseAuthorization();
 
-app.MapControllerRoute(
-                    name: "areas",
-                    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
 
-                    );
+app.UseEndpoints(endpoints =>
+        {
+          endpoints.MapControllerRoute(
+            name : "areas",
+            pattern : "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+          );
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+                });
+
+
 
 app.Run();
